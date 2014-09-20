3b800185ca681436028f0f99eafbddc231562b44
Filipe Esperandio <filipesperandio@gmail.com>
2014-09-19 22:33:44 -0300

    fix repository url

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-repository=https://github.com/filipesperandio/project-templates
+repository=https://github.com/filipesperandio/project-templates.git
 
 git archive --remote=${repository} master templates/${1} | tar xvf -
 
